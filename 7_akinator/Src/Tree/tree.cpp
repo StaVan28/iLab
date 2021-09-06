5964c1bd81363086084376d7963268a33147e526
Starchenko Ivan <starchenko.ia@phystech.edu>
2021-09-07 00:31:55 +0300

    write create_tree_from_buf

--- 7_akinator/Src/Tree/tree.cpp
+++ 7_akinator/Src/Tree/tree.cpp
@@ -31,7 +31,7 @@ void AkinatorTree::fill_akinator_tree()
 {
     Text parser_base(path_base_);
     
-    create_tree(parser_base.get_buffer_data());
+    create_tree_from_buf(parser_base.get_buffer_data());
 }
 
 //---------
@@ -43,32 +43,105 @@ void AkinatorTree::fill_akinator_base()
 
 //---------
 
-void AkinatorTree::create_tree(const char* buf_data)
+void AkinatorTree::create_tree_from_buf(const char* buf_data)
 {
-    NodeTree*   tmp_prnt_node = root_;
+    NodeTree*   tmp_new_node  = nullptr;
+    NodeTree*   tmp_prnt_node = nullptr;
     std::size_t indx_buf      = 0;
 
-    while ()
+    indx_buf++;
+
+    tmp_new_node = new NodeTree(buf_data, nullptr);
+    size_++;
+
+    root_         = tmp_new_node;
+    tmp_prnt_node = tmp_new_node;
+
+    while (!isspace(buf_data[indx_buf]))
+        indx_buf++;
+
+    while (buf_data[indx_buf] != '\0')
     {
         while (isspace(buf_data[indx_buf]))
             indx_buf++;
 
-        if (buf_data[indx_buf] == '?')
+        if (buf_data[indx_buf] == '<')
         {
             indx_buf++;
 
-            if (tree_empty())
-            {
-                root_ = new NodeTree(buf_data, nullptr);
-                size_++;
+            while (isspace(buf_data[indx_buf]))
+                indx_buf++;
 
-                tmp_prnt_node = root_;
-            }
-            else
-            { 
+            indx_buf++;
 
-            }
+            tmp_new_node = new NodeTree(buf_data, nullptr);
+            size_++;
+
+            while (!isspace(buf_data[indx_buf]))
+                indx_buf++;
+
+            tmp_new_node->parent_ = tmp_prnt_node;
+            tmp_prnt_node->left_  = tmp_new_node;
+
+            tmp_prnt_node = tmp_new_node;
         }
+
+        if (buf_data[indx_buf] == '>')
+        {
+            indx_buf++;
+
+            while (isspace(buf_data[indx_buf]))
+                indx_buf++;
+
+            indx_buf++;
+
+            tmp_new_node = new NodeTree(buf_data, nullptr);
+            size_++;
+
+            while (!isspace(buf_data[indx_buf]))
+                indx_buf++;
+
+            tmp_new_node->parent_ = tmp_prnt_node;
+            tmp_prnt_node->right_ = tmp_new_node;
+
+            tmp_prnt_node = tmp_new_node;
+        }
+
+        if (buf_data[indx_buf] == 'V')
+        {
+            indx_buf++;
+
+            tmp_prnt_node = tmp_prnt_node->parent_;
+        }
+    }
+}
+
+//---------
+
+void AkinatorTree::create_root_node(NodeTree* tmp_new_node, NodeTree* tmp_prnt_node, const char* buf_data, std::size_t indx_buf)
+{
+    indx_buf++;
+
+    tmp_new_node = new NodeTree(buf_data, nullptr);
+    size_++;
+
+    root_         = tmp_new_node;
+    tmp_prnt_node = tmp_new_node;
+
+    while (!isspace(buf_data[indx_buf]))
+        indx_buf++;
+}
+
+//---------
+
+void AkinatorTree::skip_delimiters(const char* buf_data, std::size_t indx_buf)
+{
+    indx_buf++;
+
+    while (isspace(buf_data[indx_buf]))
+        indx_buf++;
+
+    indx_buf++;
 }
 
 //---------
--- 7_akinator/Src/Tree/tree.hpp
+++ 7_akinator/Src/Tree/tree.hpp
@@ -45,17 +45,19 @@ class AkinatorTree
         void fill_akinator_tree();
         void fill_akinator_base();
 
-        void create_tree(const char* buffer_data);
+        void create_tree_from_buf(const char* buffer_data);
+        void create_root_node(NodeTree* tmp_new_node, NodeTree* tmp_prmt_node, const char* buffer_data, std::size_t indx_buf);
+        void skip_delimiters(const char* buf_data, std::size_t indx_buf);
             
         bool tree_empty() const;
         bool clear(NodeTree* clr_node);
-    
+
     public:
-        
+
         AkinatorTree() = delete;
         AkinatorTree(const std::string path_base);
        ~AkinatorTree();
-       
+
 };
 
 //--------------------------------------------------
